108ba7fd570d515e546f6e4d09c9f24830ea3ec4
Artur <nzvtrk@yandex.ru>
2017-11-27 15:59:42 +0300

    js-parallax, js-scrolllonks, js-faq, js-popup added

--- src/js/app.js
+++ src/js/app.js
@@ -6,17 +6,44 @@ $(function() {
     $('.nav-bars').toggleClass('is-active');
   });
 
-  //popups
-/*$('.popup__about').magnificPopup({
-  type: 'ajax',
-  alignTop: true,
-  overflowY: 'scroll' // as we know that popup content is tall we set scroll overflow by default to avoid jump
+//popup fullscreen
+$('.popup-with-zoom-anim').click(()=>{
+    $('body').css({'height':'100vh','overflow':'hidden'})
+    $('#small-dialog').css('display','block')
+})
+//close popup
+$('.about__close').click(()=>{
+    $('body').css({'height':'auto','overflow':'auto'})
+})
+//popup plugin
+$('.popup-with-zoom-anim').magnificPopup({
+  type: 'inline',
+  fixedContentPos: false,
+  fixedBgPos: true,
+  overflowY: 'hidden',
+  closeBtnInside: true,
+  preloader: false,
+  midClick: true,
+  removalDelay: 300,
+  mainClass: 'my-mfp-zoom-in'
 });
-*/
 
-$('.popup__about').magnificPopup({
-  type: 'ajax',
-  closeOnBgClick: false
+
+//scroll to anchor from top nav
+$("a.scrollLink").click(function () {
+  var elementClick = $(this).attr("href");
+  var destination = $(elementClick).offset().top;
+  $('html,body').animate( { scrollTop: destination }, 1100 );
+  return false;
 });
+//faq items
+$('.question').click(function() {
+  $(this).toggleClass('question-active');
+});
+//parallax
+$('.block-parallax1').parallax({imageSrc: '/img/parallax.jpg'});
+$('.block-parallax2').parallax({imageSrc: '/img/parallax2.jpg'});
+$('.block-parallax3').parallax({imageSrc: '/img/parallax3.jpg'});
+
 
 });
