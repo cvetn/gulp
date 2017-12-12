d9532538404173f28113e5b259b8a316e765e081
Artur <nzvtrk@yandex.ru>
2017-12-12 11:56:50 +0300

    fix button

--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,7 @@ $('.popup-with-zoom-anim').click(()=>{
       'overflow':'hidden',
       'background-color':'#000'
     })
+    $('.header').hide();
 
     function trans(){
       $('.mfp-wrap').css({'opacity':'1'});
@@ -30,6 +31,7 @@ $('.about__close').click(()=>{
     $('#small-dialog').css({
         'display':'none'
     });
+      $('.header').show();
 
 })
 //popup plugin
@@ -137,19 +139,53 @@ $(".swiper-pagination-bullet:nth-child(1)").text('Тренировки')
 $(".swiper-pagination-bullet:nth-child(2)").text('Питание')
 $(".swiper-pagination-bullet:nth-child(3)").text('Отчеты')
 
-var elem = document.querySelector('.personal__descr')
-var posright = document.documentElement.clientWidth - Math.floor(elem.getBoundingClientRect().right)-100;
-$('.swiper-pagination').css({
-  'left':'auto',
-  'right': posright
-})
+// var elem = document.querySelector('.personal__descr')
+// var posright = document.documentElement.clientWidth - Math.floor(elem.getBoundingClientRect().right)-100;
+// $('.swiper-pagination').css({
+//   'left':'auto',
+//   'right': posright
+// })
 
 
-$('.choose__button').click(function() {
-  $that=$(this);
-  $that.addClass('button-active');
-  $that.siblings().removeClass('button-active')
-})
+$('.choose__button').click(function () {
+
+  var $this = $(this);
+  var main = $('.choose-main');
+  var mainAdd = $('.choose-add');
+  var spec = $('.choose-special');
+  var mainChilds = main.children();
+  var mainAddChilds = mainAdd.children();
+  var specChilds = spec.children();
+
+
+  $this.addClass('button-active');
+  $this.siblings().removeClass('button-active');
+  $('.configurator__sidebar').css({'opacity':'1'})
+
+ if ($this.parent().hasClass('choose-main')){
+   mainAddChilds.removeClass('button-disabled');
+   specChilds.removeClass('button-active');
+ }
+ else if ($this.parent().hasClass('choose-add')){
+   specChilds.removeClass('button-active');
+ }
+ else if ($this.parent().hasClass('choose-special')){
+   mainAddChilds.addClass('button-disabled');
+   mainChilds.removeClass('button-active');
+   mainAddChilds.removeClass('button-active');
+ }
+ // console.log($this);
+
+  var mainId = main.children('.button-active').attr('id') || '';
+  var mainAdd = mainAdd.children('.button-active').attr('id') || '';
+  var spec = spec.children('.button-active').attr('id') || '';
+
+
+
+  var currentCourse = `${mainId}${mainAdd}${spec}`
+    console.log(currentCourse)
+
+});
 
 
 
